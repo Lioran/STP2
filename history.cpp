db368c21758055cb71f033e4fcaa180cddf6bb5f
Lioran <wersil@mail.ru>
2019-03-06 20:40:35 +0700

    Исправлен класс история

--- history.cpp
+++ history.cpp
@@ -15,29 +15,23 @@ history::history(QTableView* parent):QTableView (parent)
     this->setModel(model);
 }
 void history::record(int p1, int p2, QString num1, QString num2){
-    this->rec.p1 = p1;
-    this->rec.p2 = p2;
-    this->rec.number1 = num1;
-    this->rec.number2 = num2;
-    this->list.push_back(rec);
     QStandardItem *item;
     int row = this->model()->rowCount();
     QStandardItemModel *model = new QStandardItemModel;
     model = (QStandardItemModel*)this->model();
-    item = new QStandardItem(QString::number(this->rec.p1));
+    item = new QStandardItem(QString::number(p1));
     model->setItem(row, 0, item);
-    item = new QStandardItem(this->rec.number1);
+    item = new QStandardItem(num1);
     model->setItem(row, 1, item);
-    item = new QStandardItem(QString::number(this->rec.p2));
+    item = new QStandardItem(QString::number(.p2));
     model->setItem(row, 2, item);
-    item = new QStandardItem(this->rec.number2);
+    item = new QStandardItem(num2);
     model->setItem(row, 3, item);
     this->setModel(model);
 }
 void history::clear(){
-    this->list.clear();
     delete this->model();
 }
 int history::count(){
-    return this->list.count();
+    return this->model()->rowCount();
 }
--- history.h
+++ history.h
@@ -9,13 +9,6 @@ class history: public QTableView
 {
     Q_OBJECT
 public:
-    struct Record{
-        int p1;
-        int p2;
-        QString number1;
-        QString number2;
-    } rec;
-    QList<Record> list;
     history(QTableView* parent = nullptr);
     void record(int p1, int p2, QString num1, QString num2);
     void clear();
